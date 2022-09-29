276618248049f228dfba4dc3add0ee49749c2b9f
driss292 <drisskaci@outlook.fr>
2022-09-29 14:35:08 +0200

    index

---
+++ create.php
@@ -0,0 +1,44 @@
+<?php
+$title = "Create";
+require_once("database/connection.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $title ?></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+
+<body>
+    <h1 class="mt-5 text-center">CRUD en PHP</h1>
+    <main class="container mt-5">
+        <div class="col-md-12">
+            <h2>Création d'un article</h2>
+            <div class="col-md-6">
+                <form action="" method="post">
+                    <div class="form-group">
+                        <label for="name">Name</label>
+                        <input type="text" id="name" name="name" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="price">Price</label>
+                        <input type="text" id="price" name="price" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="stock">Stock</label>
+                        <input type="text" id="stock" name="stock" class="form-control">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </main>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
 require_once("database/connection.php");
+
+$sql = 'SELECT * FROM articles';
+$data = $db->prepare($sql);
+$data->execute();
+$articles = $data->fetchAll(PDO::FETCH_ASSOC);
+
+// var_dump($articles);
 ?>
 
 <!DOCTYPE html>
@@ -14,8 +21,41 @@ require_once("database/connection.php");
 </head>
 
 <body>
+    <h1 class="mt-5 text-center">CRUD en PHP</h1>
     <div class="container">
-        <h1 class="mt-5">CRUD en PHP</h1>
+        <div class="row">
+            <section class="mt-5">
+                <h2>Liste des articles</h2>
+                <a href="create.php" class="btn btn-primary">Ajouter un article</a>
+                <table class="table mt-3">
+                    <thead>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>price</th>
+                        <th>Stock</th>
+                        <th>Actions</th>
+                    </thead>
+
+                    <tbody>
+                        <?php foreach ($articles as $article) : ?>
+                            <tr>
+                                <td><?= $article["id"] ?></td>
+                                <td><?= $article["name"] ?></td>
+                                <td><?= $article["price"] ?></td>
+                                <td><?= $article["stock"] ?></td>
+                                <td>
+                                    <a href="#" class="btn btn-primary">Show</a>
+                                    <a href="#" class="btn btn-success">Edit</a>
+                                    <a href="#" class="btn btn-danger">Delete</a>
+                                </td>
+                            </tr>
+                        <?php endforeach ?>
+                    </tbody>
+                </table>
+
+            </section>
+        </div>
+
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
